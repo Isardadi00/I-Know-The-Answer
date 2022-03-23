612cb6447e006d6a2b6118364e6745996b3f848c
Isardadi00 <33455717+Isardadi00@users.noreply.github.com>
2022-03-23 18:45:28 +0000

    redirecting in progress

--- i_know_the_answer/src/Components/Login/index.js
+++ i_know_the_answer/src/Components/Login/index.js
@@ -1,9 +1,45 @@
-import react from "react";
+import react, { useState, useEffect } from "react";
+import { useNavigate } from "react-router"
 
 const Login = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    let navigate = useNavigate();
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log("User:", {
+            username: username,
+            password: password
+        });
+        // todo: log user into website
+    }
+
+    // navigate to register on register click
+
     return (
-        <div>
-            <h1>Login Page</h1>
+        <div className="login-page">
+            <div className="login-body">
+                <h2>Log in</h2>
+                <form onSubmit={handleSubmit}>
+                    <label>Enter Username:
+                        <input
+                            type="text"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
+                    </label>
+                    <label>Enter Password:
+                        <input
+                            type="text"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </label>
+                    <button onClick={navigate("../register")}>Register</button>
+                    <button type="submit">Log In</button>
+                </form>
+            </div>
         </div>
     );
 };
--- i_know_the_answer/src/Components/Register/index.js
+++ i_know_the_answer/src/Components/Register/index.js
@@ -1,11 +1,57 @@
-import react from "react";
+import react, { useState, useEffect } from "react";
+import { useNavigate } from "react-router"
 
 const Register = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [displayName, setDisplayName] = useState("");
+    let navigate = useNavigate();
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log("User:", {
+            username: username,
+            password: password,
+            displayName: displayName
+        });
+        // todo: create user in database
+    }
+
+    const handleCancel = () => {
+        setUsername("");
+        setPassword("");
+        setDisplayName("");
+        navigate("/login")
+    };
+
     return (
         <div className="register-page">
             <div className="register-body">
                 <h2>Register</h2>
-                <form>
+                <form onSubmit={handleSubmit}>
+                    <label>Enter Username:
+                        <input
+                            type="text"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
+                    </label>
+                    <label>Enter Password:
+                        <input
+                            type="text"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </label>
+                    <label>Enter Display Name:
+                        <input
+                            type="text"
+                            value={displayName}
+                            onChange={(e) => setDisplayName(e.target.value)}
+                        />
+                    </label>
+                    <button onClick={handleCancel}>Cancel</button>
+                    <button type="submit">Register</button>
                 </form>
             </div>
         </div>
